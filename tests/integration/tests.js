672acfc9f03ba6f97e8a3c1e8c1871926e7eaceb
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-04-02 11:32:32 +0100

    Integration tests -1 working

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -19,7 +19,10 @@ describe('calculator functionality', function() {
 
 //   1. Do the number buttons update the display of the running total?
 
-  // it("should update display with running total")
+  it("should update display with running total", function() {
+    running_total = element(by.css('#running_total'))
+    expect(running_total.getAttribute("value")).to.eventually.equal('0');
+  })
 
 // 2. Do the arithmetical operations update the display with the result of the operation?
 
@@ -34,9 +37,37 @@ describe('calculator functionality', function() {
 
 // 3. Can multiple operations be chained together?
 
-
+  it("should be able to chain operations", function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal('12');
+  })
 
 // 4. Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
+  it("should be able to output a range of numbers", function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal('-2');
+  })
+
 // 5. What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+
+  // it("should not be able to divide by zero - stop equals from working so return 0", function() {
+  //   running_total = element(by.css('#running_total'))
+  //   element(by.css('#number2')).click();
+  //   element(by.css('#operator_divide')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#operator_equals')).click();
+  //   expect(running_total.getAttribute("value")).to.eventually.equal('0');
+  // })
 
 });
